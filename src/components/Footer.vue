c7ae4f195e0d3b60e262d8afd7bc737c133bc068
lucasnevespereira <pereiraneveslucas@gmail.com>
2023-09-19 01:05:22 +0200

    add favicon and logo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,12 @@
 <template>
     <footer class="text-center py-3">
-<!--        <div class="h-10 w-10 bg-gray-200 rounded logo mx-auto"></div>-->
-        <p class="mt-2">
-           meffin.fr by
-            <a href="https://lucasnp.com" class="text-blue-500 hover:underline">lucasnp</a>
-        </p>
+        <div class="h-10 w-10 rounded logo mx-auto">
+            <img src="/logo.svg" alt="Meffin Logo" class="h-full w-full object-contain">
+        </div>
+<!--        <p class="mt-2">-->
+<!--            Meffin by-->
+<!--            <a href="https://lucasnp.com" class="text-blue-500 hover:underline">lucasnp</a>-->
+<!--        </p>-->
     </footer>
 </template>
 <script setup lang="ts">
--- src/components/nav/Logo.vue
+++ src/components/nav/Logo.vue
@@ -1,5 +1,8 @@
 <template>
-    <router-link to="/" class="normal-case text-2xl font-bold">
-        Meffin
+    <router-link to="/" class="normal-case text-2xl font-bold flex items-center">
+        <div class="h-10 w-10 lg:w-12 lg:h-12 rounded logo mx-auto flex items-center">
+            <img src="/logo.svg" alt="Meffin Logo" class="h-full w-full object-contain">
+            <span>effin</span>
+        </div>
     </router-link>
 </template>
